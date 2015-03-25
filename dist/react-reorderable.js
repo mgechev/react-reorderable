49a225180cd74cfb96a6d6107f4f7d7fe28b1524
mgechev <mgechev@gmail.com>
2015-03-25 11:53:31 -0700

    Update demo

---
+++ dist/react-reorderable.js
@@ -0,0 +1,299 @@
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['React'], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('React'));
+  } else {
+    root.ReactReorderable = factory(root.React);
+  }
+}(this, function(React) {
+function getScrollTop(node) {
+  return getScroll(node, 'scrollTop');
+}
+
+function getScrollLeft(node) {
+  return getScroll(node, 'scrollLeft');
+}
+
+function getScroll(node, prop) {
+  var result = 0;
+  while (node) {
+    result += node[prop] || 0;
+    node = node.parentNode;
+  }
+  return result;
+}
+
+function getClosestReorderable(el) {
+  while (el) {
+    if (el.className &&
+        el.className.indexOf('react-reorderable-item') >= 0) {
+      return el;
+    }
+    el = el.parentNode;
+  }
+  return null;
+}
+
+// function isItemOver(node, current) {
+//   var ax1 = node.offsetLeft;
+//   var ay1 = node.offsetTop;
+//   var ax2 = ax1 + node.offsetWidth;
+//   var ay2 = ay1 + node.offsetHeight;
+//   var bx1 = current.offsetLeft;
+//   var by1 = current.offsetTop;
+//   var bx2 = bx1 + current.offsetWidth;
+//   var by2 = by1 + current.offsetHeight;
+//   var widthIntersection = Math.max(0, Math.min(ax2, bx2) - Math.max(ax1, bx1));
+//   var heightIntersection = Math.max(0, Math.min(ay2, by2) - Math.max(ay1, by1));
+//   var intersection = widthIntersection * heightIntersection;
+//   return (intersection / (node.offsetHeight * node.offsetWidth)) >= 0.3;
+// }
+
+var SIBLING_TYPES = {
+  NONE: 0,
+  NEXT: 1,
+  PREVIOUS: 2
+};
+
+function getHorizontalSiblingType(e, node) {
+  var nodeTop = node.offsetTop - getScrollTop(node.parentNode);
+  var nodeLeft = node.offsetLeft - getScrollLeft(node.parentNode);
+  var width = node.offsetWidth;
+  var height = node.offsetHeight;
+  if (e.pageY < nodeTop || e.pageY > nodeTop + height) {
+    return SIBLING_TYPES.NONE;
+  }
+  if (e.pageX > nodeLeft && e.pageX < nodeLeft + 1 / 2 * width) {
+    return SIBLING_TYPES.NEXT;
+  }
+  if (e.pageX > nodeLeft + 1 / 2 * width && e.pageX < nodeLeft + width) {
+    return SIBLING_TYPES.PREVIOUS;
+  }
+  return SIBLING_TYPES.NONE;
+}
+
+function getVerticalSiblingType(e, node) {
+  var nodeTop = node.offsetTop - getScrollTop(node.parentNode);
+  var nodeLeft = node.offsetLeft - getScrollLeft(node.parentNode);
+  var width = node.offsetWidth;
+  var height = node.offsetHeight;
+  if (e.pageX < nodeLeft || e.pageX > nodeLeft + width) {
+    return SIBLING_TYPES.NONE;
+  }
+  if (e.pageY > nodeTop && e.pageY < nodeTop + 1 / 2 * height) {
+    return SIBLING_TYPES.NEXT;
+  }
+  if (e.pageY > nodeTop + 1 / 2 * height && e.pageY < nodeTop + height) {
+    return SIBLING_TYPES.PREVIOUS;
+  }
+  return SIBLING_TYPES.NONE;
+}
+
+function getSiblingNode(e, node, mode) {
+  var p = node.parentNode;
+  var siblings = p.children;
+  var current;
+  var done = false;
+  var result = {};
+  mode = mode || 'list';
+  for (var i = 0; i < siblings.length && !done; i += 1) {
+    current = siblings[i];
+    if (current.getAttribute('data-reorderable-key') !==
+        node.getAttribute('data-reorderable-key')) {
+      // The cursor should be around the middle of the item
+      var siblingType;
+      if (mode === 'list') {
+        siblingType = getVerticalSiblingType(e, current);
+      } else {
+        siblingType = getHorizontalSiblingType(e, current);
+      }
+      if (siblingType !== SIBLING_TYPES.NONE) {
+        result.node = current;
+        result.type = siblingType;
+        return result;
+      }
+    }
+  }
+  return result;
+}
+
+
+function indexChildren(children) {
+  var prefix = 'node-';
+  var map = {};
+  var ids = [];
+  for (var i = 0; i < children.length; i += 1) {
+    id = prefix + (i + 1);
+    ids.push(id);
+    children[i] =
+      React.createElement("div", {className: "react-reorderable-item",
+        key: id, "data-reorderable-key": id},
+        children[i]
+      );
+    map[id] = children[i];
+  }
+  return { map: map, ids: ids };
+}
+
+function is(elem, selector) {
+  var matches = elem.parentNode.querySelectorAll(selector);
+  for (var i = 0; i < matches.length; i += 1) {
+    if (elem === matches[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function getNodesOrder(current, sibling, order) {
+  var currentKey = current.getAttribute('data-reorderable-key');
+  var currentPos = order.indexOf(currentKey);
+  order.splice(currentPos, 1);
+  var siblingKey = sibling.node.getAttribute('data-reorderable-key');
+  var siblingKeyPos = order.indexOf(siblingKey);
+  if (sibling.type === SIBLING_TYPES.PREVIOUS) {
+    order.splice(siblingKeyPos + 1, 0, currentKey);
+  } else {
+    order.splice(siblingKeyPos, 0, currentKey);
+  }
+  return order;
+}
+
+var ReactReorderable = React.createClass({displayName: "ReactReorderable",
+  componentWillMount: function () {
+    var res = indexChildren(this.props.children);
+    this.setState({
+      order: res.ids,
+      reorderableMap: res.map
+    });
+    window.addEventListener('mouseup', function () {
+      this.setState({
+        mouseDownPosition: null
+      });
+    }.bind(this));
+  },
+  componentWillReceiveProps: function (nextProps) {
+    if (nextProps.children) {
+      var res = indexChildren(nextProps.children);
+      this.setState({
+        order: res.ids,
+        reorderableMap: res.map
+      });
+    }
+  },
+  getInitialState: function () {
+    return { order: [], startPosition: null, activeItem: null, reorderableMap: {} };
+  },
+  onDragStop: function (e) {
+    this.setState({
+      activeItem: null,
+      startPosition: null
+    });
+    this.props.onDrop(this.state.order.map(function (id) {
+      return this.state.reorderableMap[id].props.children;
+    }, this));
+  },
+  onDrag: function (e) {
+    var handle = this.refs.handle.getDOMNode();
+    var sibling = getSiblingNode(e, handle, this.props.mode);
+
+    if (sibling && sibling.node) {
+      var order = getNodesOrder(getClosestReorderable(handle), sibling, this.state.order);
+      this.setState({
+        order: order
+      });
+    }
+  },
+  onMouseDown: function (e) {
+    if (!this.props.handle || is(e.target, this.props.handle)) {
+      this.setState({
+        mouseDownPosition: {
+          x: e.clientX,
+          y: e.clientY
+        }
+      });
+    }
+  },
+  onMouseMove: function (e) {
+    if (!this.state.activeItem) {
+      var initial = this.state.mouseDownPosition;
+      // Still not clicked
+      if (!initial) {
+        return;
+      }
+      if (Math.abs(e.clientX - initial.x) >= 5 ||
+          Math.abs(e.clientY - initial.y) >= 5) {
+        var node = getClosestReorderable(e.target);
+        var nativeEvent = e.nativeEvent;
+        var id = node.getAttribute('data-reorderable-key');
+        // React resets the event's properties
+        this.props.onDragStart(this.state.reorderableMap[id]);
+        this.activeItem = node;
+        this.setState({
+          mouseDownPosition: null,
+          activeItem: id,
+          startPosition: {
+            x: node.offsetLeft - getScrollLeft(node),
+            y: node.offsetTop - getScrollTop(node)
+          }
+        }, function () {
+          this.refs.handle.handleDragStart(nativeEvent);
+        }.bind(this));
+      }
+    }
+  },
+  render: function () {
+    var children = this.state.order.map(function (id) {
+      var className = (this.state.activeItem) ? 'noselect ' : '';
+      if (this.state.activeItem === id) {
+        className += 'react-reorderable-item-active';
+      }
+      return React.addons.cloneWithProps(
+        this.state.reorderableMap[id], {
+          ref: 'active',
+          onMouseDown: this.onMouseDown,
+          onMouseMove: this.onMouseMove,
+          className: className
+      });
+    }, this);
+    var handle;
+    if (this.state.activeItem) {
+      var pos = this.state.startPosition;
+      handle = React.addons.cloneWithProps(
+        this.state.reorderableMap[this.state.activeItem], {
+          className: 'react-reorderable-handle'
+      });
+      handle =
+        React.createElement(ReactDrag, {onStop: this.onDragStop,
+          onDrag: this.onDrag,
+          ref: "handle",
+          start: { x: pos.x, y: pos.y}},
+          handle
+        );
+    }
+    return (
+      React.createElement("div", {ref: "wrapper"},
+        children,
+        handle
+      )
+    );
+  }
+});
+
+ReactReorderable.propTypes = {
+  onDragStart: React.PropTypes.func,
+  onDrag: React.PropTypes.func,
+  onDrop: React.PropTypes.func,
+  onChange: React.PropTypes.func
+};
+
+ReactReorderable.defaultProps = {
+  onDragStart: function () {},
+  onDrag: function () {},
+  onDrop: function () {},
+  onChange: function () {}
+};
+
+return ReactReorderable;
+}));
